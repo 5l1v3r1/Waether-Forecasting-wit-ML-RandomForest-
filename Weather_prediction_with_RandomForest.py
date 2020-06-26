f85234cf2e32daeb1b6ac821bd4de15617dc7113
berkaycan <berkaycanozcelik@gmail.com>
2020-06-26 14:38:25 +0300

    Updated

--- Weather_prediction_with_RandomForest.py
+++ Weather_prediction_with_RandomForest.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[348]:
+# In[44]:
 
 
 #Weather Prediction
 
 
-# In[349]:
+# In[45]:
 
 
 #importing Libraries
@@ -16,21 +16,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[350]:
+# In[46]:
 
 
 #reading dataset
 data = pd.read_csv(r"C:\Users\user\Desktop\IOT\weather.csv")
 
 
-# In[351]:
+# In[47]:
 
 
 #creating dataframe
 df = pd.DataFrame(data)
 
 
-# In[352]:
+# In[48]:
 
 
 #dropping nan values
@@ -38,42 +38,17 @@ df_dropped = df.dropna()
 df_dropped
 
 
-# In[353]:
-
-
-#converting the type of read_dt (object) to datetime64
-df_dropped['read_dt'] = pd.to_datetime(df_dropped['read_dt'])
-
-# Convert the date into a number (of days since some point)
-fromDate = min(df_dropped['read_dt'])
-df_dropped['timedelta'] = (df_dropped['read_dt'] - fromDate).dt.days.astype(int)
-print(df_dropped[['read_dt', 'timedelta']].head())
-
-#dropping the read_dt column which will not be used anymore ( the column timedelta will be used instead )
-df_dropped.drop('read_dt', axis = 1, inplace = True)
-
-
-# In[354]:
-
-
-#converting the read_tm column ( object ) to seconds which will be used as time for the date
-df_dropped['seconds'] = df_dropped.read_tm.apply(pd.to_timedelta).apply(lambda x: x.total_seconds())
-
-#dropping the read_tm column which will not be used anymore ( the column seconds will be used instead )
-df_dropped.drop('read_tm', axis = 1, inplace = True)
-
-
-# In[355]:
+# In[49]:
 
 
 #changing categorical data features ( Condition , wind_direciton) to numerical features that the machine can understand it ( one hot encoding )
-df_dropped  = pd.get_dummies(df_dropped,prefix=['condition','wind_direction'],columns=['condition','wind_direction'])
+df_dropped  = pd.get_dummies(df_dropped,prefix=['read_dt','read_time','condition','wind_direction'],columns=['read_dt','read_tm','condition','wind_direction'])
 
 #Dataset which will be used after data preparation
 df_dropped
 
 
-# In[356]:
+# In[50]:
 
 
 #Splitting Features and lables ( we will try to predict the 'air_quality_health_index')
@@ -82,15 +57,15 @@ X = df_dropped.drop('air_quality_health_index', axis = 1)
 y = np.array(df_dropped['air_quality_health_index'])
 
 
-# In[381]:
+# In[51]:
 
 
-#Splitting data to test and train
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+train_pct_index = int(0.80 * len(X))
+X_train, X_test = X[:train_pct_index], X[train_pct_index:]
+y_train, y_test = y[:train_pct_index], y[train_pct_index:]
 
 
-# In[382]:
+# In[52]:
 
 
 #preparing random forest
@@ -98,42 +73,46 @@ from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
 
 
-# In[383]:
+# In[53]:
 
 
 #training model
 rf.fit(X_train, y_train);
 
 
-# In[384]:
+# In[54]:
 
 
 #making predictions on test set
 predictions = rf.predict(X_test)
+predictions = predictions.round()
+#sort ettik
+#predictions = np.sort(predictions)
+#y_test = np.sort(y_test)
 
 
-# In[385]:
+# In[55]:
 
 
 #Calculating errors
 errors = abs(predictions - y_test)
 
 
-# In[386]:
+# In[56]:
 
 
 #Mean absolute error
 print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
 
 
-# In[387]:
+# In[57]:
 
 
 #Calculate mean absolute percentage error (MAPE)
 mape = 100 * (errors / y_test)
 
 
-# In[388]:
+# In[58]:
 
 
 # Calculate and display accuracy
@@ -141,28 +120,35 @@ accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
 
 
-# In[ ]:
+# In[59]:
 
 
+import matplotlib.pyplot as plt
+plt.plot(range(len(predictions)),predictions,color  = "blue")
+plt.plot(range(len(y_test)),y_test,color  = "red", alpha = 0.4)
 
 
-
-# In[ ]:
-
+# In[62]:
 
 
+#Naive bayes
+from sklearn.naive_bayes import GaussianNB 
+gnb = GaussianNB() 
+gnb.fit(X_train, y_train) 
+y_pred = gnb.predict(X_test) 
+from sklearn import metrics 
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
 
 
-# In[ ]:
+# In[65]:
 
 
-
-
-
-# In[ ]:
-
-
-
+#SVM
+from sklearn.svm import SVC 
+svm_model_linear = SVC(kernel = 'linear', C = 1).fit(X_train, y_train) 
+svm_predictions = svm_model_linear.predict(X_test)
+accuracy = svm_model_linear.score(X_test, y_test) 
+accuracy
 
 
 # In[ ]:
